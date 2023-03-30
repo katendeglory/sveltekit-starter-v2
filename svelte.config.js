0e2c66218f1e0462864229b195f51edd1178f2f3
Glory Katende <katendeglory@gmail.com>
2023-03-30 02:12:53 +0200

    Yet another commit

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
-
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
+// import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,10 +9,10 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		paths: {
-			assets: '',
-			base: ''
-		},
+		// paths: {
+		// 	assets: '',
+		// 	base: ''
+		// },
 	}
 };
 
