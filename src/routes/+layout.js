04ef4fd92c6fa8efa4f67b21f21deebd59dbab0e
Glory Katende <katendeglory@gmail.com>
2023-07-28 22:09:21 +0200

    Update +layout.js

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,2 +1,5 @@
 export const ssr = false;
+
+/* Uncomment this to use adapter static */
+/* Make sure to remove dynamic routes as well like the article directory */
 // export const prerender = true
